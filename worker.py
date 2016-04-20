8dd6d5cdfa9153ce1331d2640c64933492f0567f
alexandresebrao <alexandre.sebrao@gmail.com>
2016-04-20 15:27:36 -0300

    Funcao 1 ok

--- worker.py
+++ worker.py
@@ -3,21 +3,6 @@ import oursql
 import redis
 import datetime
 
-#Função que pega os dados do mysql e transforma em um dicionario lindo!
-def FetchOneAssoc(cursor) :
-    data = cursor.fetchone()
-    if data == None :
-        return None
-    desc = cursor.description
-
-    dict = {}
-
-    for (name, value) in zip(desc, data) :
-        dict[name[0]] = value
-
-    return dict
-
-
 # "endereço do servidor", "usuario", "senha" e "banco"
 db_mysql = oursql.connect(host="localhost",user="root",passwd="",db="aulaivo")
 
@@ -34,8 +19,34 @@ cursor = db_mysql.cursor(oursql.DictCursor)
 cursor.execute("""SELECT * FROM cad_usuario""")
 dados = cursor.fetchall()
 
+lista_cpf = []
 #Criar o Hash Redis cliente:cpf
 for cliente in dados:
-    string2 = "cpf:%s" %cliente['cpf']
-    string1 = "Nome '%s' EMAIL %s RG %s tel %s cel %s" %(cliente['nome'],cliente['email'],cliente['rg'],cliente['tel'],cliente['cel'])
-    r.hmset(string2,cliente)
+    string = "cpf:%s" %cliente['cpf']
+    r.hmset(string,cliente)
+    lista_cpf.append(cliente['cpf'])
+
+r.set('cpf',','.join(lista_cpf))
+
+############### Vamos ao menu!!!!
+def menu():
+    print "1 - Consultar Clientes"
+    print "2 - Consultar Clientes e Mostrar os Pedidos"
+    print "0 - Para sair"
+
+def opcao(valor):
+    if valor == "1":
+        r_cpf = r.get('cpf')
+        cpfs = r_cpf.split(',')
+        i = 0
+        for i in range(len(cpfs)):
+            print "%s - %s - %s" %(i+1,cpfs[i],r.hmget('cpf:'+cpfs[i],'nome')[0])
+        print "##########################################"
+
+
+while True:
+    menu()
+    i = raw_input('Escolha a opção: ')
+    if i == 0:
+        break
+    opcao(i)
