dfc1033dcfb8da43f4b8f58e19527dce0d0a173f
root <vitor.figueredo18@gmail.com>
2016-04-21 18:33:57 -0300

    Criado menu como discutido na ultima aula, reorganizado a User Interface

--- worker.py
+++ worker.py
@@ -3,15 +3,48 @@ import oursql
 import redis
 import datetime
 
+def menu():
+    r_cpf = r.get('cpf')
+    cpfs = r_cpf.split(',')
+    i = 0
+    
+    print ("-----")
+    print ("Apresentação dos dados no formato (ID - Nome - CPF)")
+    for i in range(len(cpfs)):
+        print "%s - %s - %s" %(i+1,cpfs[i],r.hmget('cpf:'+cpfs[i],'nome')[0])
+    print ("-----\n")
+
+    print "1 - Consultar Clientes e Mostrar os Pedidos"
+    print "0 - Para sair"
+def opcao(valor):
+    if valor == "1":
+        escolha = raw_input("Escolha o usuario por id: ")
+        r_cpf = r.get('cpf')
+        cpfs = r_cpf.split(',')
+        print "##########################################"
+        print "Pedido para %s" %str(r.hmget('cpf:'+cpfs[int(escolha)-1],'nome')[0])
+        print "Codigo do pedido - Data pedido - Faturado - Nao Faturado - Data Entrega"
+        pedidos = r.get('pedidos:'+cpfs[int(escolha)-1])
+        pedidos = pedidos.split(',')
+
+        if pedidos != ['']:
+            for pedido in pedidos:
+                cursor.execute("SELECT * FROM pedidos WHERE cod_pedido = %s" %pedido)
+                resultado = cursor.fetchall()
+                print "%s - %s - %s - %s - %s" %(resultado[0]['cod_pedido'],resultado[0]['dtped'],resultado[0]['faturado'],resultado[0]['naofaturado'],resultado[0]['dtentrega'])
+        else:
+            print "Nenhum pedido feito"
+        print "##########################################"
+
 #Ao início do programa, o programa espera as informacoes da base no modelo host, user, senha e db do MYSQL,
 #caso algum dado seja digitado incorretamente, avisa o erro e tenta de novo.
 #o programa então tenta conexão com o redis, caso consiga, prossegue, senão acusa o erro!
 
 while True:
-	my_SQL_host = raw_input ("Host (default: localhost): ");
-	my_SQL_user = raw_input ("User MySQL: ");
-	my_SQL_senha = raw_input ("Password MySQL: ");
-	my_SQL_db = raw_input ("Database: ");
+	my_SQL_host = raw_input ("Host MySQL (default: localhost): ")
+	my_SQL_user = raw_input ("User MySQL: ")
+	my_SQL_senha = raw_input ("Password MySQL: ")
+	my_SQL_db = raw_input ("Database MySQL: ")
 
 	try:
 		db_mysql = oursql.connect(host = my_SQL_host, user=my_SQL_user, passwd=my_SQL_senha, db=my_SQL_db)
@@ -32,71 +65,56 @@ try:
 except:
     print "Não foi possivel conectar ao Redis"
 
-##### ETAPA 1
-# Aqui precisamos popular o basico do REDIS para nossas operações
-cursor = db_mysql.cursor(oursql.DictCursor)
-cursor.execute("""SELECT * FROM cad_usuario""")
-dados = cursor.fetchall()
+print ("-----")
+print ("Selecione a questão: ")
+print ("\n1) Consultar o nome do usuário e retornar os pedidos deste usuário!")
+print ("2) Consultar o estado e retornar os usuários deste estado!")
+print ("3) Consultar o produto e retornar os dados dos produtos!")
+print ("4) Consultar pelo pedido e retornar os dados do pedido!")
+print ("5) Consultar o usuário e retornar os dados do usuário!")
+print ("6) Consultar a cidade e retornar os dados do usuário!")
+print ("7) Consultar o estado e retornar os dados de todos os logradouros deste estado!")
+print ("8) Cadastrar um novo usuário!")
+print ("9) Inserir um pedido para determinado usuário!")
 
-#Populamos os clientes
-lista_cpf = []
-#Criar o Hash Redis cliente:cpf
-for cliente in dados:
-    string = "cpf:%s" %cliente['cpf']
-    r.hmset(string,cliente)
-    lista_cpf.append(cliente['cpf'])
+escolha = input ("Digite a questão: ")
 
-r.set('cpf',','.join(lista_cpf))
+# Se a escolha for a questão 1:
+# Precisamos popular o basico do REDIS para nossas operações
+if escolha == 1:
+	print(chr(27) + "[2J")
+	cursor = db_mysql.cursor(oursql.DictCursor)
+	cursor.execute("""SELECT * FROM cad_usuario""")
+	dados = cursor.fetchall()
+	lista_cpf = []
 
-#populamos os Pedidos
-cpfs = r.get('cpf').split(',')
-for cpf in cpfs:
-    cursor.execute("SELECT cod_pedido FROM pedidos WHERE cad_usuario_cpf = %s" %(cpf))
-    dados = cursor.fetchall()
-    lista_pedido = []
-    if len(dados) > 0:
-        for dado in dados:
-            lista_pedido.append(str(dado['cod_pedido']))
-        r.set('pedidos:'+cpf,','.join(lista_pedido))
-    else:
-        r.set('pedidos:'+cpf,'')
+	#Criar o Hash Redis cliente:cpf
+	for cliente in dados:
+		string = "cpf:%s" %cliente['cpf']
+		r.hmset(string,cliente)
+		lista_cpf.append(cliente['cpf'])
 
-############### Vamos ao menu!!!!
-def menu():
+	r.set('cpf',','.join(lista_cpf))
 
-    r_cpf = r.get('cpf')
-    cpfs = r_cpf.split(',')
-    i = 0
-    print "Id, Nome, e CPF"
-    for i in range(len(cpfs)):
-        print "%s - %s - %s" %(i+1,cpfs[i],r.hmget('cpf:'+cpfs[i],'nome')[0])
-    print "##########################################"
+	#populamos os Pedidos
+	cpfs = r.get('cpf').split(',')
+	for cpf in cpfs:
+		cursor.execute("SELECT cod_pedido FROM pedidos WHERE cad_usuario_cpf = %s" %(cpf))
+		dados = cursor.fetchall()
+		lista_pedido = []
+		if len(dados) > 0:
+			for dado in dados:
+				lista_pedido.append(str(dado['cod_pedido']))
+			r.set('pedidos:'+cpf,','.join(lista_pedido))
+		else:
+			r.set('pedidos:'+cpf,'')
 
-    print "1 - Consultar Clientes e Mostrar os Pedidos"
-    print "0 - Para sair"
+	############### Vamos ao menu!!!!
 
-def opcao(valor):
-    if valor == "1":
-        escolha = raw_input("Escolha o usuario por id: ")
-        r_cpf = r.get('cpf')
-        cpfs = r_cpf.split(',')
-        print "##########################################"
-        print "Pedido para %s" %str(r.hmget('cpf:'+cpfs[int(escolha)-1],'nome')[0])
-        print "Codigo do pedido - Data pedido - Faturado - Nao Faturado - Data Entrega"
-        pedidos = r.get('pedidos:'+cpfs[int(escolha)-1])
-        pedidos = pedidos.split(',')
 
-        if pedidos != ['']:
-            for pedido in pedidos:
-                cursor.execute("SELECT * FROM pedidos WHERE cod_pedido = %s" %pedido)
-                resultado = cursor.fetchall()
-                print "%s - %s - %s - %s - %s" %(resultado[0]['cod_pedido'],resultado[0]['dtped'],resultado[0]['faturado'],resultado[0]['naofaturado'],resultado[0]['dtentrega'])
-        else:
-            print "Nenhum pedido feito"
-        print "##########################################"
-while True:
-    menu()
-    i = raw_input('Escolha a opção: ')
-    if i == "0":
-        break
-    opcao(i)
+	while True:
+		menu()
+		i = raw_input('Escolha a opção: ')
+		if i == "0":
+			break
+		opcao(i)
