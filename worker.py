9be9854b0e04ceafeb64403357b41f82f6056fe9
root <vitor.figueredo18@gmail.com>
2016-04-24 20:31:49 -0300

    Adicionada outras opcões de menu e comeco da implementacao da questão 2

--- worker.py
+++ worker.py
@@ -3,29 +3,32 @@ import oursql
 import redis
 import datetime
 
-def menu():
+def menu_questao1():
+	
     r_cpf = r.get('cpf')
     cpfs = r_cpf.split(',')
     i = 0
-    
+    print ("Apresentação dos dados no formato 'ID - Nome - CPF': \n")
     print ("-----")
-    print ("Apresentação dos dados no formato (ID - Nome - CPF)")
     for i in range(len(cpfs)):
         print "%s - %s - %s" %(i+1,cpfs[i],r.hmget('cpf:'+cpfs[i],'nome')[0])
     print ("-----\n")
 
     print "1 - Consultar Clientes e Mostrar os Pedidos"
-    print "0 - Para sair"
-def opcao(valor):
+    print "0 - Para sair\n"
+    
+def opcao_questao1(valor):
     if valor == "1":
         escolha = raw_input("Escolha o usuario por id: ")
         r_cpf = r.get('cpf')
         cpfs = r_cpf.split(',')
-        print "##########################################"
+        print(chr(27) + "[2J")
+        print "\n-----\n"
         print "Pedido para %s" %str(r.hmget('cpf:'+cpfs[int(escolha)-1],'nome')[0])
         print "Codigo do pedido - Data pedido - Faturado - Nao Faturado - Data Entrega"
         pedidos = r.get('pedidos:'+cpfs[int(escolha)-1])
         pedidos = pedidos.split(',')
+        print "\n-----\n"
 
         if pedidos != ['']:
             for pedido in pedidos:
@@ -34,20 +37,28 @@ def opcao(valor):
                 print "%s - %s - %s - %s - %s" %(resultado[0]['cod_pedido'],resultado[0]['dtped'],resultado[0]['faturado'],resultado[0]['naofaturado'],resultado[0]['dtentrega'])
         else:
             print "Nenhum pedido feito"
-        print "##########################################"
+        print "\n-----"
 
 #Ao início do programa, o programa espera as informacoes da base no modelo host, user, senha e db do MYSQL,
-#caso algum dado seja digitado incorretamente, avisa o erro e tenta de novo.
-#o programa então tenta conexão com o redis, caso consiga, prossegue, senão acusa o erro!
+#Caso algum dado seja digitado incorretamente, avisa o erro e tenta de novo.
+#O programa então tenta conexão com o redis, caso consiga, prossegue, senão acusa o erro!
 
 while True:
-	my_SQL_host = raw_input ("Host MySQL (default: localhost): ")
+	my_SQL_host = raw_input ("Host MySQL (default: localhost): ")	
 	my_SQL_user = raw_input ("User MySQL: ")
 	my_SQL_senha = raw_input ("Password MySQL: ")
 	my_SQL_db = raw_input ("Database MySQL: ")
 
+	##fiz só para não ficar digitando o tempo todo que to testando!
+	if my_SQL_host == "admin_vitor":
+		my_SQL_host = "localhost"
+		my_SQL_user = "root"
+		my_SQL_senha = "root"
+		my_SQL_db = "aulaivo"
+	
 	try:
 		db_mysql = oursql.connect(host = my_SQL_host, user=my_SQL_user, passwd=my_SQL_senha, db=my_SQL_db)
+		print(chr(27) + "[2J")
 		print ("\n-----")
 		print ("Conexão realizada com sucesso!")
 		print ("-----\n")
@@ -76,12 +87,14 @@ print ("6) Consultar a cidade e retornar os dados do usuário!")
 print ("7) Consultar o estado e retornar os dados de todos os logradouros deste estado!")
 print ("8) Cadastrar um novo usuário!")
 print ("9) Inserir um pedido para determinado usuário!")
+print ("0) Sair")
+print ("-----")
+escolha = input ("\nDigite a questão: ")
 
-escolha = input ("Digite a questão: ")
-
-# Se a escolha for a questão 1:
-# Precisamos popular o basico do REDIS para nossas operações
+## Se a escolha for a questão 1:
+## Precisamos popular o basico do REDIS para nossas operações
 if escolha == 1:
+	
 	print(chr(27) + "[2J")
 	cursor = db_mysql.cursor(oursql.DictCursor)
 	cursor.execute("""SELECT * FROM cad_usuario""")
@@ -113,8 +126,56 @@ if escolha == 1:
 
 
 	while True:
-		menu()
+		menu_questao1()
 		i = raw_input('Escolha a opção: ')
 		if i == "0":
 			break
-		opcao(i)
+		opcao_questao1(i)
+
+## Se a escolha for a questão 2:
+## Precisamos popular o REDIS com as informarcoes sobre os usuarios em um determinado Estado!
+elif escolha == 2:
+	
+
+	print(chr(27) + "[2J")
+	cursor = db_mysql.cursor(oursql.DictCursor)
+	
+	## Para apresentacao o cursor ira percorrer e apresentar todas as opcoes de estado!
+	cursor.execute("""SELECT * FROM uf""")
+	dados_estado = cursor.fetchall()
+	lista_estado = []
+	estado_index = 0
+	
+	print "-----"
+	for estado in dados_estado:
+		estado_index += 1
+		lista_estado.append(['ds_uf_nome'])
+		print "%d - %s" %(estado_index, estado['ds_uf_nome'])
+		
+	print "-----\n"
+	estado_escolha = input ("Por favor, digite o código do estado que deseja buscar: ")
+
+
+elif escolha == 3:
+	print "Não implementado ainda"
+
+elif escolha == 4:
+	print "Não implementado ainda"
+
+elif escolha == 5:
+	print "Não implementado ainda"
+	
+elif escolha == 6:
+	print "Não implementado ainda"
+	
+elif escolha == 7:
+	print "Não implementado ainda"
+
+elif escolha == 8:
+	print "Não implementado ainda"
+	
+elif escolha == 9:
+	print "Não implementado ainda"
+	
+elif escolha == 9:
+	quit ()
